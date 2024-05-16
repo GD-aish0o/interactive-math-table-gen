a6b036c4ccdb085d1d2bbf25cbfef2fea55e7c15
Aqsaa Qaazi <159919683+GD-aish0o@users.noreply.github.com>
2024-05-16 19:04:11 +0500

    Added files.
    Run "npx math-table-loop" to test the project on your CLI. otherwise here is the main code in TS.

---
+++ main.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+// A typescript "foor loop" for a multiply table on any number provided by the user.
+
+// Interactive multiply tables loop.
+
+import inquirer from "inquirer"
+
+const table = await inquirer.prompt([
+    {
+        name: "TableNumber",
+        type : "number",
+        message: "Enter the digit you want the table of."
+    }
+]);
+const count = 12;
+
+for (let i = 1; i <= count; i++) {
+    console.log(`${table.TableNumber} X ${i} = ${table.TableNumber * i}`);
+}
