9fc6d78d18fa12f8b0544b327bfcce9220c0ab8b
Aqsaa Qaazi <159919683+GD-aish0o@users.noreply.github.com>
2024-05-17 01:20:30 +0500

    updated user input for times of table

---
+++ loop-4.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+// A typescript "foor loop" for a multiply table on any number provided by the user.
+// Interactive multiply tables loop.
+import inquirer from "inquirer";
+const table = await inquirer.prompt([
+    {
+        name: "TableNumber",
+        type: "number",
+        message: "Enter the digit you want the table of."
+    }
+]);
+// how many times a table should be printed:
+// 12 ;)  c
+const count = await inquirer.prompt([
+    {
+        name: "TableCount",
+        type: "number",
+        message: "Enter a number of times you want the table to be printed. "
+    }
+]);
+for (let i = 1; i <= count.TableCount; i++) {
+    console.log(`${table.TableNumber} X ${i} = ${table.TableNumber * i}`);
+}
